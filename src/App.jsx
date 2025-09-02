65d58e51474ccfdd2ea13d0b4c2d708eb16273b1
Lykos-79 <kumarabhisekh0111@gmail.com>
2025-09-02 15:13:05 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./components/Home";
 import Work from "./components/Work";
-import Services from "./components/services";
+import Services from "./components/Services";
 import Careers from "./components/Careers";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
